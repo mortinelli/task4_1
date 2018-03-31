aa057372cd756e47322dceded5bad77a83f0b3b5
mortinelli <seer_os@mail.ru>
2018-03-31 18:44:23 +0300

    Add task4_1.sh

---
+++ task4_1.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+exec 1> task4_1.out
+echo --- Hardware ---
+cpu=$(lscpu | grep "Model name:" | cut -d ":" -f2 | sed 's/^ *//;s/ *$//')
+ram=$(cat /proc/meminfo | grep "MemTotal" | cut -d ":" -f2 | sed 's/^ *//;s/ *$//')
+motherboard=$(dmidecode -s baseboard-product-name)
+sysserialnum=$(dmidecode -s system-serial-number)
+osdistr=$(cat /etc/*-release | grep PRETTY_NAME | grep -o '".*"'| sed 's/"//g')
+kernel=$(uname -mrs)
+installdate=$(ls -alct / | tail -1| awk '{print $6, $7, $8}')
+uptimeDays=$(uptime -p | cut -d' ' -f2-)
+numberOfProcesses=$(ps aux --no-heading | wc -l)
+userLoggedIn=$(who | wc -l)
+exec 1>> task4_1.out
+echo CPU: $cpu
+echo RAM: $ram
+echo Motherboard: ${motherboard:-"Unknow"}
+echo System Serial Number: ${sysserialnum:-"Unknow"}
+echo --- System ---
+echo OS Distribution: $osdistr
+echo Kernel version: $kernel
+echo Installation date: $installdate
+echo "Hostname:" $(hostname) 
+echo Uptime: $uptimeDays
+echo Processes running: $numberOfProcesses
+echo User logged in: $userLoggedIn
+echo --- Network ---
+ip -o link show | awk  -F': ' '{print $2}' > /tmp/ifacess.txt
+ip -o -f inet addr show | awk '{print $2":",$4}'> /tmp/ips.txt
+uniq /tmp/ifacess.txt /tmp/unicfacess.txt
+ifaces=$(cat /tmp/unicfacess.txt)
+for var in $ifaces
+do
+ips=$(grep $var /tmp/ips.txt | cut -d':' -f2)
+ipstr=""
+for ip in $ips
+do
+ipstr+=$ip," "
+done
+if [ -z "$ipstr" ]; then
+echo $var: -
+else
+echo $var: ${ipstr::-2}
+fi
+done
+
